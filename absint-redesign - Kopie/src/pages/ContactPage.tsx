37b1228dc634c9bb0695575a9ca82f1134c3fb8b
MikaSbrt <siebertmika@yahoo.com>
2026-04-07 19:32:25 +0200

    Delete absint-redesign - Kopie directory

--- absint-redesign - Kopie/src/pages/ContactPage.tsx
+++
@@ -1,9 +0,0 @@
-import ContactSection from '../components/home/ContactSection'
-
-export default function ContactPage() {
-  return (
-    <div className="pt-20 min-h-screen bg-background">
-      <ContactSection />
-    </div>
-  )
-}
